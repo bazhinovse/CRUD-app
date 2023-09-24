d1e4100e0fd333c01f9492ce7eedb51f9ce050a4
bazhinovse <bazhinovse@gmail.com>
2023-09-24 20:17:29 +0300

    First CRUD app

---
+++ src/main/java/project/dao/PersonDAO.java
@@ -0,0 +1,41 @@
+package project.dao;
+
+import org.springframework.stereotype.Component;
+import project.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static  int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people=new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT,"Elya"));
+        people.add(new Person(++PEOPLE_COUNT,"Vova"));
+        people.add(new Person(++PEOPLE_COUNT,"Stas"));
+    }
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId()==id).findAny().orElse(null);
+    }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson ){
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id){
+        people.removeIf(p ->p.getId()==id);
+    }
+}
